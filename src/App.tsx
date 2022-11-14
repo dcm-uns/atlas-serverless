437b916e89474f56ef42b8cf28ceb3d1f7368787
Diego <dcm@cs.uns.edu.ar>
2022-11-14 15:07:38 -0300

    Inicial

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {useState} from 'react';
+import './App.css';
+import * as Realm from "realm-web";
+
+
+const app = new Realm.App({ id: "APPID" });
+
+
+// Create a component that displays the given user's details
+const UserDetail = ({ user }: { user: Realm.User }) => {
+  return (
+    <div>
+      <h1>Logged in with anonymous id: {user.id}</h1>
+    </div>
+  );
+};
+
+// Create a component that lets an anonymous user log in
+type LoginProps = {
+  setUser: (user: Realm.User) => void;
+};
+
+
+const Login = ({ setUser }: LoginProps) => {
+  const loginAnonymous = async () => {
+    const user: Realm.User = await app.logIn(Realm.Credentials.anonymous());
+    setUser(user);
+  };
+  return <button onClick={loginAnonymous}>Log In</button>;
+};
+
+
+
+const App = () => {
+  // Keep the logged in Realm user in local state. This lets the app re-render
+  // whenever the current user changes (e.g. logs in or logs out).
+  const [user, setUser] = React.useState<Realm.User | null>(app.currentUser);
+
+  // If a user is logged in, show their details. Otherwise, show the login screen.
+  return (
+    <div className="App">
+      <div className="App-header">
+        {user ? <UserDetail user={user} /> : <Login setUser={setUser} />}
+      </div>
+    </div>
+  );
+};
+
+export default App;
